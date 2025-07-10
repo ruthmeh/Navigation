3702d43d75b2d9d8851511dbd2f9202ed6522866
Ruth Mehreteab <ruthmehreteab12@gmail.com>
2025-07-10 10:01:26 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ruth Mehreteab on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
